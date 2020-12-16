b8f3d8813de429f8a63113c5310b03a22c6e2bd8
Lee Coupe <lee.coupe@immediate.co.uk>
2020-12-16 20:54:23 +0000

    added notes to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Load the express module
 var express = require('express')
 var app = express()
 
+// Define request response in root url and serve response
 app.get('/', function (req, res) {
   res.send('Hello World! - SRE Role')
 })
 
+// Launch server on port 8080 and log to console
 app.listen(8080, function () {
   console.log('app listening on port 8080!')
 })
