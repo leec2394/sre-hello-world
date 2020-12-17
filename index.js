d0a63dd3ecbd25030fbefb053bd36062c8889e74
Lee Coupe <lee.coupe@immediate.co.uk>
2020-12-17 15:05:00 +0000

    revert test change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 
 // Define request response in root url and serve response
 app.get('/', function (req, res) {
-  res.send('Hello World! - SRE Role - changed 12:50')
+  res.send('Hello World! - SRE Role')
 })
 
 // Launch server on port 8080 and log to console
